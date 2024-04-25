fe07b6e857e8d781ef1f929515b0aac7016efb27
NurSultan56 <nurhuseynov56@gmail.com>
2024-04-25 16:22:43 +0400

    JS DONE

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,29 @@
-// const buttonRUB = document.getElementById('from-rub')
-// const buttonUSD = document.getElementById('from-usd')
-// const buttonEUR = document.getElementById('from-eur')
-// const buttonGBP = document.getElementById('from-gbp')
-
-
-// buttonRUB.addEventListener('click', () => {
-//     buttonRUB.classList.toggle("button-selected")
-// })
-
 
 const fromInput = document.getElementById('from-amount-input')
 const resultInput = document.getElementById('result-amount-input')
 
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const getCurrencyData = async () => {
     const buttonSelectedFrom = document.querySelector('#from-container .button-selected').textContent
     const buttonSelectedTo = document.querySelector('#to-container .button-selected').textContent
-    switch (buttonSelectedFrom) {
-        case buttonSelectedTo:
+    console.log(buttonSelectedFrom)
+    console.log(buttonSelectedTo)
+    if (buttonSelectedFrom === buttonSelectedTo) {
             resultInput.value = fromInput.value
             const currencyDescription = document.querySelectorAll('.currency-description')
 
-            currencyDescription.forEach((item)=>{
+            currencyDescription.forEach((item) => {
                 item.textContent = `1 ${buttonSelectedFrom} = 1 ${buttonSelectedTo}`
             })
-            break
-        default:
-            const response = await fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_iLRJhIPjbHbjjs867JpDVDpiMjrfWXDD3AuI2s6K&currencies=${buttonSelectedTo}&base_currency=${buttonSelectedFrom}`)
+    } else {
+            await timeout(100)
+            console.log('ok')
+            const response = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${buttonSelectedFrom.toLowerCase()}.min.json`)
             const data = await response.json()
-            return data
+            return data[`${buttonSelectedFrom.toLowerCase()}`]
 
     }
 }
@@ -35,13 +31,41 @@ const getCurrencyData = async () => {
 const getInverseCurrencyData = async () => {
     const buttonSelectedFrom = document.querySelector('#from-container .button-selected').textContent
     const buttonSelectedTo = document.querySelector('#to-container .button-selected').textContent
-    const response = await fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_iLRJhIPjbHbjjs867JpDVDpiMjrfWXDD3AuI2s6K&currencies=${buttonSelectedFrom}&base_currency=${buttonSelectedTo}`)
+    await timeout(100)
+    const response = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024.4.24/v1/currencies/${buttonSelectedTo.toLowerCase()}.min.json`)
     const data = await response.json()
-    return data
+    return data[`${buttonSelectedTo.toLowerCase()}`]
 }
 
 
+fromInput.addEventListener('keyup', () => {
+    getCurrencyData()
+        .then((data) => {
+            switch (fromInput.value) {
+                case '':
+                    resultInput.value = 0
+                    break
+                default:
+                    const buttonSelectedTo = document.querySelector('#to-container .button-selected').textContent
+                    const buttonSelectedFrom = document.querySelector('#from-container .button-selected').textContent
+                    resultInput.value = Number(fromInput.value) * data[`${buttonSelectedTo.toLowerCase()}`]
+                    const currencyDescriptionFrom = document.getElementById('currency-description-from')
+                    currencyDescriptionFrom.textContent = `1 ${buttonSelectedFrom} = ${data[buttonSelectedTo.toLowerCase()]} ${buttonSelectedTo}`
 
+                    getInverseCurrencyData()
+                        .then((data) => {
+                            const currencyDescriptionTo = document.getElementById('currency-description-to')
+                            currencyDescriptionTo.textContent = `1 ${buttonSelectedTo} = ${data[buttonSelectedFrom.toLowerCase()]} ${buttonSelectedFrom}`
+                        })
+                        .catch(error => {
+                            console.log(error)
+                        })
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        })
+})
 
 let [flag1, flag2] = [false, false]   //FLAGS
 
@@ -59,6 +83,33 @@ buttonFrom.forEach((item) => {
             item.classList.remove("button-selected")
             flag1 = true
         }
+
+        getCurrencyData()
+        .then((data) => {
+            switch (fromInput.value) {
+                case '':
+                    resultInput.value = 0
+                    break
+                default:
+                    const buttonSelectedTo = document.querySelector('#to-container .button-selected').textContent
+                    const buttonSelectedFrom = document.querySelector('#from-container .button-selected').textContent
+                    resultInput.value = Number(fromInput.value) * data[`${buttonSelectedTo.toLowerCase()}`]
+                    const currencyDescriptionFrom = document.getElementById('currency-description-from')
+                    currencyDescriptionFrom.textContent = `1 ${buttonSelectedFrom} = ${data[buttonSelectedTo.toLowerCase()]} ${buttonSelectedTo}`
+
+                    getInverseCurrencyData()
+                        .then((data) => {
+                            const currencyDescriptionTo = document.getElementById('currency-description-to')
+                            currencyDescriptionTo.textContent = `1 ${buttonSelectedTo} = ${data[buttonSelectedFrom.toLowerCase()]} ${buttonSelectedFrom}`
+                        })
+                        .catch(error => {
+                            console.log(error)
+                        })
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        })
     })
 })
 
@@ -77,37 +128,30 @@ buttonResult.forEach((item) => {
             item.classList.remove("button-selected")
             flag2 = true
         }
+
+        getCurrencyData()
+        .then((data) => {
+            switch (fromInput.value) {
+                case '':
+                    resultInput.value = 0
+                    break
+                default:
+                    const buttonSelectedTo = document.querySelector('#to-container .button-selected').textContent
+                    const buttonSelectedFrom = document.querySelector('#from-container .button-selected').textContent
+                    resultInput.value = Number(fromInput.value) * data[`${buttonSelectedTo.toLowerCase()}`]
+                    const currencyDescriptionFrom = document.getElementById('currency-description-from')
+                    currencyDescriptionFrom.textContent = `1 ${buttonSelectedFrom} = ${data[buttonSelectedTo.toLowerCase()]} ${buttonSelectedTo}`
+
+                    getInverseCurrencyData()
+                        .then((data) => {
+                            const currencyDescriptionTo = document.getElementById('currency-description-to')
+                            currencyDescriptionTo.textContent = `1 ${buttonSelectedTo} = ${data[buttonSelectedFrom.toLowerCase()]} ${buttonSelectedFrom}`
+                        })
+                        .catch(error => {
+                            console.log(error)
+                        })
+            }
+        })
+        .catch(error => {console.log(error)})
+        })
     })
-})
-
-
-fromInput.addEventListener('keyup', () => {
-    setTimeout(getCurrencyData()
-    .then((data) => {
-        const buttonSelectedTo = document.querySelector('#to-container .button-selected').textContent
-        resultInput.value = Number(fromInput.value) * data.data[`${buttonSelectedTo}`]
-        const buttonSelectedFrom = document.querySelector('#from-container .button-selected').textContent
-        const currencyDescriptionFrom = document.getElementById('currency-description-from')
-        currencyDescriptionFrom.textContent = `1 ${buttonSelectedFrom} = ${data.data[buttonSelectedTo]} ${buttonSelectedTo}`
-
-        getInverseCurrencyData()
-            .then((data) => {
-                const currencyDescriptionTo = document.getElementById('currency-description-to')
-                currencyDescriptionTo.textContent = `1 ${buttonSelectedTo} = ${data.data[buttonSelectedFrom]} ${buttonSelectedFrom}` 
-            })
-            .catch(error => {
-                alert(error=`Вы превысили лимит допустимых обращений! (Так как используемый API бесплатный, имеется определенный лимит). Пожалуйста побробуйте обратиться чуть позже
-
-
-You reached limit of call! (Since the API used is free, there is a limit on calls). Please try later!`)
-            })
-    .catch(error => {
-        alert(error=`Вы превысили лимит допустимых обращений! (Так как используемый API бесплатный, имеется определенный лимит). Пожалуйста побробуйте обратиться чуть позже
-
-
-
-You reached limit of call! (Since the API used is free, there is a limit on calls). Please try later!`)
-    })
-        
-    }) ,2000)  
-})
